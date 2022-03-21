ac06e10f27d3e8c7a37133801ad8d71464b9a53c
Arkashu <kopeikaarkashu@gmail.com>
2022-03-21 16:36:33 +0200

    update

---
+++ js/calculations.js
@@ -0,0 +1,5 @@
+let first = prompt("Введите первое число");
+let second = prompt("Введите второе число");
+let result = first * second;
+
+alert("Ваш результат: " + result);
---
+++ js/username.js
@@ -0,0 +1,5 @@
+let name = prompt("Введите Ваше имя");
+let Surname = prompt("Введите Вашу фамилию");
+let Secondname = prompt("Введите Ваше отчество");
+let Fullname = name + " " + Surname + " " + Secondname;
+alert("Вас зовут: " + Fullname);
